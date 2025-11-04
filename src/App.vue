e527662f71761914344d8dceda3a702318fb42a6
qinkai <937817514@qq.com>
2025-11-04 17:35:19 +0800

    style(App): 添加滚动条样式
    - 为 Webkit 浏览器添加自定义滚动条样式
    - 设置滚动条宽度和高度为6px
    - 滚动条背景设为透明
    - 滚动条滑块背景设为半透明白色
    - 滚动条滑块圆角设为 10px

--- src/App.vue
+++ src/App.vue
@@ -124,4 +124,17 @@ body {
   @include hover-highlight-text()
 }
 
+// 滚动条样式
+::-webkit-scrollbar {
+  --size: 6px;
+  width: var(--size);
+  height: var(--size);
+  background-color: transparent;
+}
+::-webkit-scrollbar-thumb {
+  background-color: rgba(255, 255, 255, 0.3);
+  border-radius: 10px;
+}
+
+
 </style>
